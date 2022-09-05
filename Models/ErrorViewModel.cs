74f90d1aa0abd29acd3f874890d737d29ce3637a
kevinmunguia31 <kevinmunguia373@gmail.com>
2022-09-04 18:56:24 -0600

    Iniciando Proyecto

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace MvcBrowserJobs.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
