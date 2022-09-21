b3f8e0b6bd07e789db88692e2390d7fc52253df1
kevinmunguia31 <kevinmunguia373@gmail.com>
2022-09-20 21:49:44 -0600

    Cambio del Fondo de la Interfaz

---
+++ Models/Jobs.cs
@@ -0,0 +1,14 @@
+namespace MvcBrowserJobs.Models;
+
+public class Jobs
+{
+    public int Id {get; set;}
+    public String? Nombre {get; set;}
+    public String? Municipio {get; set;}
+    public String? Departamento {get; set;}
+    public String? Categoria {get; set;}
+    public String? Profesion {get; set;}
+    public String? Perfil {get; set;}
+    public int Estrellas {get; set;}
+
+}
