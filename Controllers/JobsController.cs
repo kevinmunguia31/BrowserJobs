89c348041c3bc4c31c58d1f2d34443199f70319e
kevinmunguia31 <kevinmunguia373@gmail.com>
2022-09-22 22:07:07 -0600

    Funcionalidad del Buscador y Details

--- Controllers/JobsController.cs
+++ Controllers/JobsController.cs
@@ -1,13 +1,46 @@
-using System.Diagnostics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
-/* using MvcBrowserJobs.Models; */
+using MvcBrowserJobs.Models;
+using Newtonsoft.Json;
 
-namespace MvcBrowserJobs.Controllers;
-
-public class JobsController : Controller
+namespace BrowserJobs.Controllers
 {
-    public IActionResult Index()
+    public class JobsController : Controller
     {
-        return View();
+        public List<Jobs> lstJobs = null;
+        public JobsController()
+        {
+            var myJsonString = System.IO.File.ReadAllText("Models/Jobs.json");
+            lstJobs = JsonConvert.DeserializeObject<List<Jobs>>(myJsonString);
+        }
+
+        public IActionResult Index()
+        {
+            return View(lstJobs);
+        }
+
+        public IActionResult Find(string profesion, string departamento, string municipio)
+        {
+            List<Jobs> lstResultJobs = new List<Jobs>();
+            foreach (var item in lstJobs)
+            {
+                if(item.Profesion.ToLower().Contains(profesion.ToLower()) || item.Departamento.ToLower().Contains(departamento.ToLower()) || item.Municipio.ToLower().Contains(municipio.ToLower()))
+                    lstResultJobs.Add(item);
+            }
+            return View("Index", lstResultJobs);
+        }
+
+        public IActionResult Details(int id){
+
+            foreach (var item in lstJobs)
+            {
+                if(item.Id == id)
+                    return View(item);
+            }
+            return View(new Jobs());
+        }
     }
 }
